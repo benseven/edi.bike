51e8833ec64d0596ceb669c28c792c62942f2046
Ben Seven <ben@kin.works>
2023-10-24 21:37:29 +0100

    cow

--- functions/insert/insert.js
+++ functions/insert/insert.js
@@ -1,11 +1,24 @@
-// Create a single supabase client for interacting with your database
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const handler = async (event) => {
+  try {
+    
+    // Create a single supabase client for interacting with your database
 
-const { createClient } = require('@supabase/supabase-js');
+    const { createClient } = require('@supabase/supabase-js');
 
-// This URL and anon public key are both found in Project > Home > Project API at Supabase
-const supabaseUrl = "https://jtsqoijulrjlpdmpugnz.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0c3FvaWp1bHJqbHBkbXB1Z256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgxNzMyNTEsImV4cCI6MjAxMzc0OTI1MX0.K1qIrb9MwClhvfpq9luLQBMwQKXzbdNzojj_WGD69W8";
+    // This URL and anon public key are both found in Project > Home > Project API at Supabase
+    const supabaseUrl = "https://jtsqoijulrjlpdmpugnz.supabase.co";
+    const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0c3FvaWp1bHJqbHBkbXB1Z256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgxNzMyNTEsImV4cCI6MjAxMzc0OTI1MX0.K1qIrb9MwClhvfpq9luLQBMwQKXzbdNzojj_WGD69W8";
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+    const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log('Supabase Instance: ', supabase);
+    console.log('Supabase Instance: ', supabase);
+
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
+
+
