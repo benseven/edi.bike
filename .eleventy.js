ece85babfdf4a89a75164038ab2d47df22fb41c8
Ben Seven <ben@kin.works>
2025-02-19 12:53:58 +0000

    Pass over utm_source metadata to Buttondown from signup form, with dictionary in site-js/sources.js to track which URL param is which marketing method

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   // Copy `css/fonts/` to `_site/css/fonts`
   // Keeps the same directory structure.
   eleventyConfig.addPassthroughCopy("site-css");
+  eleventyConfig.addPassthroughCopy("site-js");
 
   // Return your Object options:
   return {
---
+++ site-js/sources.js
@@ -0,0 +1,9 @@
+const sources = [
+  "none",                                                         //0 
+  "A5 Laminate flyer/poster QR code (public and bike shops)",     //1
+  "NFC tag or sticker (public)",                                  //2
+  "NFC tag or sticker (bike shop register)",                      //3
+  "Bike rack sticker (80mmW portrait or smaller reprint) QR code",//4
+  "9cm 'Subscribe' Stickers QR Code" ,                            //5
+  "Square Business Card QR Code",                                 //6
+]
